c5f569179f9728c1d2458b97dc5189ac14feab28
Gabriela Zalta <gzalta96@gmail.com>
2020-11-20 12:12:42 -0500

    working on five day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ const savedStuff = localStorage.getItem("whatever") || []
 var displayWeather = function() {
 
     let cityName = document.querySelector("#searchbar").value
-    const currentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    const currentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`;
 
     $.ajax({
         url:currentUrl,
@@ -14,11 +14,11 @@ var displayWeather = function() {
     }).then(function(initial){
         console.log(initial);
         $.ajax({
-            url:`https://api.openweathermap.org/data/2.5/onecall?lat=${initial.coord.lat}&lon=${initial.coord.lon}&exclude=hourly,alerts,minutely&appid=${apiKey}`
+            url:`https://api.openweathermap.org/data/2.5/onecall?lat=${initial.coord.lat}&lon=${initial.coord.lon}&exclude=hourly,alerts,minutely&units=imperial&appid=${apiKey}`
         }).then(function(feedback){
             //current weather
             console.log(feedback);
-            $("#city").append(initial.name);
+            $("#city").append(initial.name + " " + "(" + (moment().format('L')) + ")");
             $("#temperature").append(initial.main.temp + "°F");
             $("#humidity").append(initial.main.humidity + "%");
             $("#wind").append(initial.wind.speed + " MPH");
@@ -26,14 +26,18 @@ var displayWeather = function() {
 
             //five day forecast
             for(i=0;i<5;i++){
-                $("#fiveDay").append(`<div class=" col col-sm-6 card text-white bg-primary mb-3 ml-3">
-                <div class="card-text"></div>
-                <div class="card-body">
-                    <p class="card-text">emoji</p>
-                    <p class="card-text">Temp:(((${feedback.daily[i].temp.day}) - 273.15) * 1.8 + 32) °F </p>
+                $("#fiveDay").append(`
+                <div class="col card text-white bg-primary ml-2 mb-3 mt-2 p-2 rounded text-left">
+                <div class="card-text">
+                        <h6 id="date" class="font-weight-bold"> ${feedback.daily[i].dt} </h6>
+                </div>
+                <div class="card-body p-2">
+                    <p class="card-text">${feedback.daily[i].weather.weather[i].icon}</p>
+                    <p class="card-text">Temp: ${feedback.daily[i].temp.day} °F </p>
                     <p class="card-text">Humidity: ${feedback.daily[i].humidity}%</p>
                 </div>
-              </div>`)
+              </div>
+              `)
             }
         })
     })
